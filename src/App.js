2e5e568c9e5380cd42a2989d356890bc921880e0
Cluyie <steffen_witt@hotmail.com>
2020-06-23 15:52:20 +0200

    Lection 40-50(before assignment)Events, state

--- src/App.js
+++ src/App.js
@@ -8,18 +8,56 @@ class App extends Component {
       {name: 'Max', age: 28},
       {name: 'Manu', age: 29},
       {name: 'Steffen', age: 28}
-    ]
+    ],
+    otherState: 'Some other value'
+  }
+
+  switchNameHandler = (newName) => {
+    // DONT DO THIS: this.state.persons[0].name = 'Steffen Juhl';
+    this.setState( {
+      persons: [
+      {name: newName, age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Steffen', age: 38}
+      ] 
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState( {
+      persons: [
+      {name: 'Max', age: 28},
+      {name: event.target.value, age: 29},
+      {name: 'Steffen', age: 38}
+      ] 
+    })
   }
   
   render() {
+    const style = {
+      backgroundColor:  'white',
+      font: 'inherit',
+      border:'1px solid blue',
+      padding:'8px',
+      cursor: 'pointer'
+    };
     return (
       <div className="App">
         <h1>Hi, iam react App</h1>
         <p>this is realle working!</p>
-        <button>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button style={style}
+        onClick={() => this.switchNameHandler('Hans!!')}>Switch Name</button> {/* () => this.switchNameHandler er en fuction. Når det står på en linje vil alt efter => blive returnet som om der stod => return somecode. Man kan også gøre det normalt ved at tilføje { retrun somecode} */}
+        <Person
+         name={this.state.persons[0].name} 
+         age={this.state.persons[0].age} />
+        <Person 
+         name={this.state.persons[1].name} 
+         age={this.state.persons[1].age}
+         click={this.switchNameHandler.bind(this, 'Hans')}
+         changed={this.nameChangedHandler}>My hobbies: Racing</Person>
+        <Person 
+         name={this.state.persons[2].name} 
+         age={this.state.persons[2].age}/>
       </div>
     );
     /* return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'does this work now?') ); */
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Person.css';
 
 const person  = (props) => {
     return (
-       <div>
-           <p>Iam {props.name} and i am {props.age} years old!</p>
+       <div className ="Person" >
+           <p onClick={props.click}>Iam {props.name} and i am {props.age} years old!</p>
             <p>{props.children}</p> {/* Children bliver brugt til at render alt det der står mellem opening og closening tag af vores component Så når vi i app.js bruger denne component vil vi kunne putte noget ind imellem <Person>noget ind her </Person>*/}
+            <input type="text" onChange={props.changed} value = {props.name}/>
         </div> 
     
     )
